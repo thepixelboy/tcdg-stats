ebe9974ebf4cf60edc769c85ae85b864fd5829ec
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-02-18 17:08:03 +0100

    Running an analysis

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
 import fs from 'fs';
+
+// Reading the CSV file and parsing it
+// into an array of arrays
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8'
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
